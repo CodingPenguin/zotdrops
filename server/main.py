610c76757a338e704d2403ddfd6ad7456b37bcaa
CodingPenguin <nguyenhuydanny@gmail.com>
2021-11-20 13:23:56 -0800

    Added /drop

--- server/main.py
+++ server/main.py
@@ -1,44 +1,59 @@
 from flask import Flask
 from flask import request
 from flask_cors import CORS
-from collections import defaultdict
 
 app = Flask(__name__)
 CORS(app)
 
-users = {}
+drops_dict: dict = {}
+
+# request.form = {
+#     'date': '',
+#     'time': '',
+#     'name': '',
+#     'location_name': '',
+#     'location_coords': ''
+# }
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
-
+  
+@app.route("/drop", methods=['POST'])
+def drop():
+    date = request.form['date']
+    time = request.form['time']
+    dropper_name = request.form['dropperName']
+    location_name = request.form['locationName']
+    location_coordinates = request.form['locationCoords']
+    
+    drops_dict[date] = Drop(time, dropper_name, location_name, location_coordinates)
+        
+    
 '''TODO MAP STUFF'''
 
 # calendar of upcoming petrdrops
 
 #key = name of dropper, value = Drop(time, dropper_name, location_name, location_coordinates)
-class drop:
+class Drop:
     def __init__(self, time, dropper_name, location_name, location_coordinates):
         self.time = time
         self.dropper = dropper_name
         self.location_name = location_name
         self.location_coordinates = location_coordinates
     
-    def get_time():
+    def get_time(self):
         return self.time
     
-    def get_dropper_name():
+    def get_dropper_name(self):
         return self.dropper
 
-    def get_location_name():
+    def get_location_name(self):
         return self.location_name
 
-    def get_location_coordinates():
+    def get_location_coordinates(self):
         return self.location_coordinates
 
-
-
-drop_dict = {}
 
 
 # pin current drop locations
