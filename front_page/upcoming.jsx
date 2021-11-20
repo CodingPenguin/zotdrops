a52b1f68c0a994593901a3e2525edcc50586cb67
Mia Schroeder <miaschroeder527@gmail.com>
2021-11-20 15:24:10 -0800

    Started creating UpcomingDrops component.

---
+++ front_page/upcoming.jsx
@@ -0,0 +1,19 @@
+class UpcomingDrops extends React.Component {
+    render() {
+        <div>
+            <h1> 
+                Upcoming Drops
+            </h1>
+        </div>
+    }
+}
+
+class CalenderEntries extends React.Component {
+    render() {
+        <div> 
+            <h2> 
+                
+            </h2>
+        </div>
+    }
+}
