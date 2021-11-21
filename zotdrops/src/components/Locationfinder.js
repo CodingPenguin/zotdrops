39dddde8ad22d7fe424a46226913fb698c35897f
CodingPenguin <nguyenhuydanny@gmail.com>
2021-11-20 22:20:18 -0800

    Yes good morning

--- zotdrops/src/components/Locationfinder.js
+++ zotdrops/src/components/Locationfinder.js
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMapEvent } from 'react-leaflet'
 
-function Locationfinder() {
-    const [data, setData] = useState([[51.509, -0.09]]);
+function Locationfinder({mapData, setMapData}) {
+    // const [data, setData] = useState([[51.509, -0.09]]);
     const map = useMapEvent('click', (e) => {
       console.log(e.latlng);
       //send request to server
-      setData([...data, e.latlng]);
+      // setData([...data, e.latlng]);
+      setMapData([...mapData, e.latlng]);
       //send e.latlng to server
       // fetch(serverUrl + '/addDrop?lat=' + 50 + 'lng=' + 23)
     })
@@ -18,7 +19,7 @@ function Locationfinder() {
     </Marker>
     }
     return <>
-        {data.map(hi)}
+        {mapData.map(hi)}
     </>
   }
 
