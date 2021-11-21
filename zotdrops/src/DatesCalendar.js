ac2cad3c9cfd7d0f8fdf52ed5bd2b175ed12d1cf
Mia Schroeder <miaschroeder527@gmail.com>
2021-11-20 22:08:49 -0800

    CALM DOWN CARO

--- zotdrops/src/DatesCalendar.js
+++ zotdrops/src/DatesCalendar.js
@@ -1,7 +1,10 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import UpcomingDate from './UpcomingDate'
+import axios from 'axios';
 
 function DatesCalendar() {
+
+    const [dates, setDates] = useState([])
 
     let mockObj = {
         "_id": "11/20/21",
@@ -19,34 +22,59 @@ function DatesCalendar() {
         ]
     }
 
-    let dates = [mockObj, mockObj, mockObj]
     let formattedDates = []
 
-    // [[dropDate, dropTime, dropperName], [dropDate, dropTime, dropperName], ...]
-    const getDrop = () => { 
-        for (let index = 0; index < dates.length; index++) {
-            let dropDate = dates[index]["_id"]
+    function getDropDates(){
+        console.log('working')
+        axios.get('http://127.0.0.1:5000/drops')
+        .then((response) => {
+            let serverData = []
+          response = response['data']
+          console.log(response)
+          let today = new Date(),
+          todayDate = (today.getMonth() + 1) + '/'  + today.getDate() + '/' + today.getFullYear();
+          for (let i = 0; i < response.length; i++) {
+            let dropDate = response[i]['_id']
+            if (dropDate !== todayDate) {
+                for (let j = 0; j < response[i]['drops'].length; j++) {
+                    let dropName = response[i]['drops'][j]['dropper_name']
+                    let dropTime = response[i]['drops'][j]['time']
+                    serverData.push([dropDate, dropTime, dropName])
+                }
             
-            for (let innerIndex = 0; innerIndex < dates[index]["drops"].length; innerIndex++) {
-                let dropTime = dates[index]["drops"][innerIndex]["time"]
-                let dropperName = dates[index]["drops"][innerIndex]["dropper_name"]
-
-                formattedDates.push([dropDate, dropTime, dropperName])
             }
-        }
-    }
+            
+          }
+          setDates(serverData)
+        })
+      }
 
-    getDrop()
+    // [[dropDate, dropTime, dropperName], [dropDate, dropTime, dropperName], ...]
+    // const getDrop = () => { 
+    //     for (let index = 0; index < dates.length; index++) {
+    //         let dropDate = dates[index]
+            
+    //         for (let innerIndex = 0; innerIndex < dates[index]["drops"].length; innerIndex++) {
+    //             let dropTime = dates[index]["drops"][innerIndex]["time"]
+    //             let dropperName = dates[index]["drops"][innerIndex]["dropper_name"]
 
+    //             formattedDates.push([dropDate, dropTime, dropperName])
+    //         }
+    //     }
+    // }
+
+    // getDropDates()
+    useEffect(getDropDates, [])
     let combinedDates = [];
-
-    for (let i = 0; i < formattedDates.length; i++) {
-        combinedDates.push(<UpcomingDate date={formattedDates[i][0]} time = {formattedDates[i][1]} petrSticker = {formattedDates[i][2]} />)
+    
+    for (let i = 0; i < dates.length; i++) {
+        combinedDates.push(<UpcomingDate date={dates[i][0]} time = {dates[i][1]} petrSticker = {dates[i][2]} />)
     }
+
 
     return ( 
-        <div className = "upcoming-drops-comp">
-            <h2 className = "header2">Upcoming Drops</h2>
+        <>
+            <h2>Upcoming Drops</h2>
             <table class="drop-table">
                 <thead>
                     <th scope = "col" class = "table-header"> Date</th>
@@ -57,7 +85,7 @@ function DatesCalendar() {
                     {combinedDates}
                 </tbody>
             </table>
-        </div>
+        </>
     );
 }
 
