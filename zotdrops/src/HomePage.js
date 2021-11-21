1bcb8739ed91077ebc01950b7e4e06314d05e0a9
Mia Schroeder <miaschroeder527@gmail.com>
2021-11-20 18:58:01 -0800

    Created LiveDrop component.

--- zotdrops/src/HomePage.js
+++ zotdrops/src/HomePage.js
@@ -2,10 +2,12 @@ import React from "react";
 import DatesCalendar from './DatesCalendar';
 import About from './About';
 import AreYouPetr from './AreYouPetr';
+import LiveDrop from './LiveDrop';
 
 function HomePage() {
     return (
         <div>
+            <LiveDrop />
             <DatesCalendar />
             <About />
             <AreYouPetr />
---
+++ zotdrops/src/LiveDrop.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function LiveDrop() {
+    return (
+        <div>
+            <h2>LIVE: Today's Drop</h2>
+            <h4>Time: 2:00PM</h4>
+            <h4>Location: ?</h4>
+        </div>
+    );
+}
+
+export default LiveDrop
