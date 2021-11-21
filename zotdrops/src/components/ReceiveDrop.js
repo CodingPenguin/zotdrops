c4db7acd562818734a1be6a933f69db7e66974ab
CodingPenguin <nguyenhuydanny@gmail.com>
2021-11-20 19:02:49 -0800

    update

--- zotdrops/src/components/ReceiveDrop.js
+++ zotdrops/src/components/ReceiveDrop.js
@@ -1,11 +1,16 @@
 import { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMapEvent } from 'react-leaflet'
+import axios from 'axios';
 
 function ReceiveDrop() {
     const [positions, setPositions] = useState([]);
     useEffect(getUpdatedPositions, [])
 
     function getUpdatedPositions(){
+        axios.get('http://127.0.0.1:5000/drops')
+        .then((response) => {
+          console.log(response)
+        })
         let serverData = [[0, 0]];
         setPositions(serverData);
        
